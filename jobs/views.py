001c77327a28003df2ea4b2d8b5f6fad25c8c4d9
fractalswift <matthewthomasmacklin@gmail.com>
2020-02-01 20:17:38 +0000

    customised homepage

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
-from .models import Job
+from .models import Job, Scan
 
 def home(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    #scan = Scan.btc_balance
+    #btc_balance = Scan.btc_balance
+
+    #test_object = "I'm a test object"
+
+    hello = Scan.print_hello()
+    balance = Scan.show_balance()
+    orderbook = Scan.get_current_btc_price()
+
+  
+    return render(request,'jobs/home.html', {'jobs': jobs, 'hello': hello, 'balance':balance, 'orderbook':orderbook} )
+    #return render(request, 'jobs/home.html', {'jobs':jobs, 'btc_balance':btc_balance, 'test_object':test_object})
+
+
+
