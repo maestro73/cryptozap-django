ddaaf20b97a85a2676424bc1f32f041c9f0cd563
fractalswift <matthewthomasmacklin@gmail.com>
2020-02-02 09:48:18 +0000

    removed ftx from jobs views and models

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from . import ftx
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
@@ -17,24 +16,4 @@ class Scan(models.Model):
     def print_hello():
 
         return "hello there pirate"
-
-    def show_balance():
-
-        client = ftx.FtxClient()
-        balances = client.get_balances()
-
-        return balances[0].get('total')
-
-    def get_current_btc_price():
-
-        client = ftx.FtxClient()
-        btc_price = client.get_orderbook('BTC-PERP')
-
-        return btc_price
-
-    #def get_websocket_ticker():
-
-        #wsclient = ftx_websockets.FtxWebsocketClient()
-        #ticker = wsclient.get_ticker('LTC-PERP')
-
 
--- jobs/views.py
+++ jobs/views.py
@@ -10,11 +10,10 @@ def home(request):
     #test_object = "I'm a test object"
 
     hello = Scan.print_hello()
-    balance = Scan.show_balance()
-    orderbook = Scan.get_current_btc_price()
+   
 
   
-    return render(request,'jobs/home.html', {'jobs': jobs, 'hello': hello, 'balance':balance, 'orderbook':orderbook} )
+    return render(request,'jobs/home.html', {'jobs': jobs, 'hello': hello} )
     #return render(request, 'jobs/home.html', {'jobs':jobs, 'btc_balance':btc_balance, 'test_object':test_object})
 
 
